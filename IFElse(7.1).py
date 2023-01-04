78af884de204713a7ba642e9ee2dacb86cbaa435
AayushParikh1909 <121938578+AayushParikh1909@users.noreply.github.com>
2023-01-04 15:44:15 +0530

    Add files via upload

---
+++ IFElse(7.1).py
@@ -0,0 +1,8 @@
+x = int(input("Enter any number "))
+
+if(x==0):
+    print("0 is neither negative nor positive number")
+elif(x>0):
+    print("It is a positive number")
+else:
+    print("It is a negative number")

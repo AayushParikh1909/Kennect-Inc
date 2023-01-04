06b982700876fc242adf200be71627bf6116b7cb
AayushParikh1909 <121938578+AayushParikh1909@users.noreply.github.com>
2023-01-04 17:26:32 +0530

    Add files via upload

---
+++ switch(7.1).c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter any number between 0 to 6\n");
+    scanf("%d",&n);
+    switch(n)
+    {
+        case 0:
+        printf("Sunday");
+        break;
+
+        case 1:
+        printf("Monday");
+        break;
+        
+        case 2:
+        printf("Tuesday");
+        break;
+        
+        case 3:
+        printf("Wednesday");
+        break;
+        
+        case 4:
+        printf("Thursday");
+        break;
+        
+        case 5:
+        printf("Friday");
+        break;
+        
+        case 6:
+        printf("Saturday");
+        break;
+        
+        default:
+        printf("Number is not between 0-6");
+
+    }
+    return 0;
+}
